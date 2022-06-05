5e9aba6a80e1d0ad695e914e58518f0cf36bc69b
Dimitar Ganev <d.ganev@protonmail.ch>
2022-06-06 00:49:47 +0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -3,28 +3,26 @@
 apt-get update
 
 # ---------------------------------------------------------------------------
-# Repositories
+# Repositories                                                              
+# ---------------------------------------------------------------------------
 apt install software-properties-common -y
 add-apt-repository 'ppa:deadsnakes/ppa' -y
+apt-add-repository 'ppa:fish-shell/release-3' -y
 
-# Docker
-
+# ---------------------------------------------------------------------------
+# Docker Setup
+# ---------------------------------------------------------------------------
 apt-get install  ca-certificates gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-
-# ---------------------------------------------------------------------------
-# Docker Setup
-# ---------------------------------------------------------------------------
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -q -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # ---------------------------------------------------------------------------
 # Python Setup
-# https://launchpad.net/~deadsnakes/+archive/ubuntu/ppa
 # ---------------------------------------------------------------------------
 apt install python-dev -q -y
 apt-get install python3.8 python3.9 python3.10 -q -y
@@ -33,6 +31,13 @@ apt-get install python3.8 python3.9 python3.10 -q -y
 # Rust Setup
 # ---------------------------------------------------------------------------
 curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+# ---------------------------------------------------------------------------
+# Go Setup
+# ---------------------------------------------------------------------------
+wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz 
+tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+rm -f go1.18.3.linux-amd64.tar.gz
 
 # ---------------------------------------------------------------------------
 # Git Setup
@@ -52,7 +57,6 @@ cat > /etc/gitconfig <<EOF
 [credential "https://gitlab.com"]
         provider = generic
 EOF
-
 
 # ---------------------------------------------------------------------------
 # Tooling
